f87f40e256c68017f0b5798fd051232e0b58d3e3
Nicolas Venegas <zx128kok@gmail.com>
2020-09-19 23:18:00 -0400

    more default gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,8 +31,25 @@ module.exports = {
       resolve: "gatsby-plugin-google-tagmanager",
       options: {
         id: "GTM-M699BXV",
+
+        // Include GTM in development.
+        //
+        // Defaults to false meaning GTM will only be loaded in production.
         includeInDevelopment: false,
+
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        //
+        // Defaults to null
         defaultDataLayer: { platform: "gatsby" },
+
+        // Specify optional GTM environment details.
+
+        // Name of the event that is triggered
+        // on every Gatsby route change.
+        //
+        // Defaults to gatsby-route-change
+        routeChangeEventName: "YOUR_ROUTE_CHANGE_EVENT_NAME",
       },
     },
   ],
